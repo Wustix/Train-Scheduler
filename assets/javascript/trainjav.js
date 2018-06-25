5a6237303ec6e776c47e68ba1768158d8d1e729f
unknown <wroehrman@yahoo.com>
2018-06-25 11:36:20 -0500

    updates to Train files

--- assets/javascript/trainjav.js
+++ assets/javascript/trainjav.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
 
+    var config = {
+        apiKey: "AIzaSyASPFDoIz5lFw4YbE9egZNWw3l3r5w7D7A",
+        authDomain: "trainschedule-8e2ee.firebaseapp.com",
+        databaseURL: "https://trainschedule-8e2ee.firebaseio.com",
+        projectId: "trainschedule-8e2ee",
+        storageBucket: "",
+        messagingSenderId: "753309814253"
+    };
+
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
 
 
 
@@ -14,5 +26,4 @@ $(document).ready(function () {
 
 
 
-    
 }); 
